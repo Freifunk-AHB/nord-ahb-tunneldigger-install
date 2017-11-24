c359eb6a0114432cefd00bd371dc15f85d23588b
Tarnatos <tarnatos@posteo.de>
2017-11-24 20:27:57 +0100

    Update l2tp-install-2_per_hand.sh

--- l2tp-install-2_per_hand.sh
+++ l2tp-install-2_per_hand.sh
@@ -1,3 +1,6 @@
-cd /srv/tunneldigger ; virtualenv tunneldigger ; source bin/activate
+cd /srv/
+virtualenv tunneldigger
+cd tunneldigger
+source bin/activate
 pip install -r broker/requirements.txt
 exit
