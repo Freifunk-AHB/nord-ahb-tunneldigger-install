9253f955cd82e6607fb52c309886a0c43a1c85ca
ahb-3 <admin@freifunknord.de>
2017-11-27 21:33:41 +0100

    init Scripte via wget

--- l2tp-install-4.sh
+++ l2tp-install-4.sh
@@ -1,38 +1,12 @@
 #!/bin/bash
 
 ## Start Scripte anlegen
-touch /srv/tunneldigger/broker/scripts/session-up.sh
-chmod +x /srv/tunneldigger/broker/scripts/session-up.sh
-cat <<-EOF>> /srv/tunneldigger/broker/scripts/session-up.sh
-#!/bin/bash
+cd /srv/tunneldigger/broker/scripts/
+rm session-up.sh
+rm session-pre-down.sh
+wget
+wget
 
-INTERFACE="$3"
-UUID="$8"
-
-log_message() {
-      message="$1"
-      logger -p 6 -t "Tunneldigger" "$message"
-      echo "$message" | systemd-cat -p info -t "Tunneldigger"
-      echo "$1" 1>&2
-}
-
-if /bin/grep -Fq $UUID /srv/tunneldigger/broker/blacklist; then
-      log_message "New client with UUID=$UUID is blacklisted, not adding to tunneldigger bridge interface"
-else
-      log_message "New client with UUID=$UUID connected, adding to tunneldigger bridge interface"
-      ip link set dev $INTERFACE up mtu 1364
-      /sbin/brctl addif tunneldigger $INTERFACE
-fi
-EOF
-
-touch /srv/tunneldigger/broker/scripts/session-pre-down.sh
-chmod +x /srv/tunneldigger/broker/scripts/session-pre-down.sh
-cat <<-EOF>> /srv/tunneldigger/broker/scripts/session-pre-down.sh
-#!/bin/bash
-INTERFACE="$3"
-/sbin/brctl delif tunneldigger $INTERFACE
-exit 0
-EOF
 
 ## iptables Regeln anlegen
 touch /etc/iptables.d/500-Allow-tunneldigger
